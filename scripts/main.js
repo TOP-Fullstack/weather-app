1870380e2770a16da170194b560fe283a9f83ab1
Kevan <kevan.haggins@gmail.com>
2023-02-01 20:00:39 -0800

    API call + very basic search functionality

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+/* 
+TO-FIX:
+    - Search input is sending form data when the user presses enter
+    - Create object to store all needed info
+    - Re-factor code
+    - Add loading component
+    - Add styling
+    - Push to GH
+*/
+
+const button = document.querySelector("input[type=button]");
+
+async function getWeather(searchvalue) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchvalue}&appid=${API}`;
+  const response = await fetch(url);
+  const test = await response.json();
+  console.log(test);
+}
+
+button.addEventListener("click", () => {
+  const search = document.querySelector("input[type=text]").value;
+  getWeather(search);
+});
